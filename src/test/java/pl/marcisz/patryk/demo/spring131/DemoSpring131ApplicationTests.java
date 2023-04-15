077c545098e5e3fddae1bd0d299f4118382e2deb
Patryk <patryk.marcisz.v2@gmail.com>
2023-04-15 10:40:52 +0200

    prototyp dependency injector

--- src/test/java/pl/marcisz/patryk/demo/spring131/DemoSpring131ApplicationTests.java
+++ src/test/java/pl/marcisz/patryk/demo/spring131/DemoSpring131ApplicationTests.java
@@ -1,10 +1,15 @@
 package pl.marcisz.patryk.demo.spring131;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.marcisz.patryk.demo.spring131.repository.TranslationRepository;
 
 @SpringBootTest
 class DemoSpring131ApplicationTests {
+
+	@Autowired
+	TranslationRepository translationRepository;
 
 	@Test
 	void contextLoads() {
