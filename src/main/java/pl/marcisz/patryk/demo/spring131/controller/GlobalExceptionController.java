3b6b0b47c49212c5155c594db793b0dbc2301ec4
Patryk <patryk.marcisz.v2@gmail.com>
2023-04-22 11:14:26 +0200

    pokazanie ze przeniesienie @ExceptionHandler do klasy oznaczonej jako @RestControllerAdvice, powoduje ze nawet jesli controller nie posiada wlasnego @ExceptionHandler dla konkretnej klasy, moze korzystac z jego 'globalnej' wersji

---
+++ src/main/java/pl/marcisz/patryk/demo/spring131/controller/GlobalExceptionController.java
@@ -0,0 +1,34 @@
+package pl.marcisz.patryk.demo.spring131.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.marcisz.patryk.demo.spring131.model.dto.ExceptionResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleValidationError(MethodArgumentNotValidException argumentNotValidException){
+        StringBuilder errorMessage = new StringBuilder("Błędy podczas walidacji: ");
+
+        for (FieldError fieldError : argumentNotValidException.getBindingResult().getFieldErrors()) {
+            errorMessage.append(fieldError.getField()).append(" ").append(fieldError.getDefaultMessage()).append(" | ");
+        }
+        return new ExceptionResponse(errorMessage.toString());
+    }
+
+
+}
