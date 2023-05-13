5702e84e33452bb2c7963854a550fe6e4fb9eb09
Patryk <patryk.marcisz.v2@gmail.com>
2023-05-13 14:40:16 +0200

    tworzenie nowego elementu

--- src/main/java/pl/marcisz/patryk/demo/spring131/controller/mvc/MvcPopulationController.java
+++ src/main/java/pl/marcisz/patryk/demo/spring131/controller/mvc/MvcPopulationController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.marcisz.patryk.demo.spring131.population.model.dto.Population;
 import pl.marcisz.patryk.demo.spring131.population.service.PopulationService;
 
@@ -25,6 +27,13 @@ public class MvcPopulationController {
         List<Population> allPopulations = populationService.getAllPopulations();
         log.info("pobrano {} elementow", allPopulations.size());
         model.addAttribute("populations", allPopulations);
+        model.addAttribute("groupForm", new Population());
         return "main";
+    }
+
+    @PostMapping("/")
+    public String addItem(@ModelAttribute Population groupForm){
+        populationService.savePopulation(groupForm);
+        return "redirect:/";
     }
 }
